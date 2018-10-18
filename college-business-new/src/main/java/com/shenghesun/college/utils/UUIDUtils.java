7adde3a9be1245042e6cd7f53929302476c2c081
kevin <kevin@kevin-TM1701>
2018-10-18 14:27:55 +0800

    kevin 配置测试环境

--- college-business-new/src/main/java/com/shenghesun/college/utils/UUIDUtils.java
+++ college-business-new/src/main/java/com/shenghesun/college/utils/UUIDUtils.java
@@ -9,7 +9,7 @@ public class UUIDUtils {
 	}
 	
 	public static void main(String[] args) {
-		for(int i = 0 ; i < 10 ; i ++) {
+		for(int i = 0 ; i < 84 ; i ++) {
 			System.out.println(getUUId());
 		}
 	}
